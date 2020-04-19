fc2f61801153a7dab352601d4bdd16c698145ab3
Anderson Montañez <anderson.montanez@globant.com>
2020-04-18 22:36:58 -0400

    Adding client consumption

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
+import { Offer } from './model/Offer';
 import { Component, ViewChild } from '@angular/core';
 import { } from 'googlemaps';
+import { OffersService } from './offers.service';
 
 @Component({
   selector: 'app-root',
@@ -11,13 +13,38 @@ export class AppComponent {
   map: google.maps.Map;
   
   title = 'ofrecelo-frontend';
+  
+  constructor(private offersService: OffersService) {
+
+  }
+
+  private offers: Array<Offer>;
 
   ngOnInit() {
+
+    this.offersService.getOffers().subscribe(
+      offers => {
+        console.log(JSON.stringify(offers))
+        this.offers
+      } 
+    );
+
+    var myLatLng = {lat: -33.4041511, lng: -70.5570019};
+
     var mapProp = {
-      center: new google.maps.LatLng(-33.4041511, -70.5570019),
+      center: new google.maps.LatLng(myLatLng),
       zoom: 15,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
+
     this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+
+
+    var marker = new google.maps.Marker({
+      position: myLatLng,
+      map: this.map,
+      draggable: true,
+      title: 'My title'
+    });
   }
 }
--- src/app/offers.service.ts
+++ src/app/offers.service.ts
@@ -1,11 +1,18 @@
+import { Offer } from './model/Offer';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OffersService {
 
-  constructor() { 
-  
+  constructor(private http: HttpClient) { 
+    
+  }
+
+  getOffers(): Observable<Array<Offer>> {
+    return this.http.get<Array<Offer>>("http://localhost:8080/offer");
   }
 }

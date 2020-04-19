ae4db1c985432709374fe9d765b9c13ff6ab9c8f
Anderson Montañez <anderson.montanez@globant.com>
2020-04-19 15:18:09 -0400

    Implementing creating user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,23 @@
+import { AuthenticationService } from './_services/authentication.service';
 import { Coordinates } from './model/coordinates';
 import { Offer } from './model/offer';
 import { Component, ViewChild } from '@angular/core';
 import { } from 'googlemaps';
 import { OffersService } from './_services/offers.service';
 import { Observable, Subject } from 'rxjs';
+import { User } from './model/user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {  
+export class AppComponent {
+  currentUser: User;
+  
   title = 'ofrecelo-frontend';
   
-  constructor() {
-
+  constructor(private authenticationService: AuthenticationService) {
+    this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
   }
 }

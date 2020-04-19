77e77cd60942cbe9cf98a7e8d68b5f9509deeae2
Anderson Montañez <anderson.montanez@globant.com>
2020-04-18 21:25:43 -0400

    Adding map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('gmap') gmapElement: any;
+  map: google.maps.Map;
+  
   title = 'ofrecelo-frontend';
+
+  ngOnInit() {
+    var mapProp = {
+      center: new google.maps.LatLng(-33.403468, -70.556611),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+  }
 }

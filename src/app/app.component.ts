ca791a4bc613cd6e1dc783cbcb2f74fa60064d96
Anderson Montañez <anderson.montanez@globant.com>
2020-04-19 01:11:24 -0400

    Adding geospatial assignation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
+import { Coordinates } from './model/Coordinates';
 import { Offer } from './model/Offer';
 import { Component, ViewChild } from '@angular/core';
 import { } from 'googlemaps';
 import { OffersService } from './offers.service';
+import { Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +12,7 @@ import { OffersService } from './offers.service';
 })
 export class AppComponent {
   @ViewChild('gmap') gmapElement: any;
-  map: google.maps.Map;
+  map: Subject<google.maps.Map> = new Subject();
   
   title = 'ofrecelo-frontend';
   
@@ -20,11 +22,30 @@ export class AppComponent {
 
   private offers: Array<Offer>;
 
+  coordinates = new Subject<Coordinates>();
+
   ngOnInit() {
 
-    
+    navigator.geolocation.getCurrentPosition(pos => {
+      let coord = new Coordinates();
+      coord.latitude = pos.coords.latitude;
+      coord.longitude = pos.coords.longitude;
+      this.coordinates.next(coord)
+    });
 
-    var myLatLng = {lat: -33.4041511, lng: -70.5570019};
+    this.coordinates.subscribe(c => {
+      var myLatLng = {lat: c.latitude, lng: c.longitude};
+      var mapProp = {
+        center: new google.maps.LatLng(myLatLng),
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+
+      let myMap = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+      this.map.next(myMap);
+    });
+
+    var myLatLng = {lat: 0, lng: 0};
 
     var mapProp = {
       center: new google.maps.LatLng(myLatLng),
@@ -32,23 +53,28 @@ export class AppComponent {
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
 
-    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+    let myMap = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
 
-    this._offersService.getOffers().subscribe(
-      offers => {
-        offers.map(
-          offer => {
-            const marker = new google.maps.Marker({
-              position: {lat: offer.coordinates.latitude, lng: offer.coordinates.longitude},
-              map: this.map,
-              draggable: true,
-              title: offer.title
-            });
-            this.createInfoWindow(marker, offer);
-          }
-        );
-      } 
-    );
+    this.map.next(myMap);
+
+    this.map.subscribe(m => {
+      this._offersService.getOffers().subscribe(
+        offers => {
+          offers.map(
+            offer => {
+              const marker = new google.maps.Marker({
+                position: {lat: offer.coordinates.latitude, lng: offer.coordinates.longitude},
+                map: m,
+                draggable: true,
+                title: offer.title
+              });
+              this.createInfoWindow(marker, offer);
+            }
+          );
+        } 
+      );
+    });
+    
   }
 
   createInfoWindow(marker: google.maps.Marker, offer: Offer) {
@@ -70,4 +96,6 @@ export class AppComponent {
       infoWindow.open(marker.get('map'), marker);
     });
   } 
+
+
 }

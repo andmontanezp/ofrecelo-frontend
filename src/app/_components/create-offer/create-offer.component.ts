884cab416653ce2bcd4826eefe768485b85fec41
Josse Niño <jossenino@gmail.com>
2020-05-01 18:52:29 -0400

    JN - Modifying the offer page and connecting it to the backend

--- src/app/_components/create-offer/create-offer.component.ts
+++ src/app/_components/create-offer/create-offer.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { FormGroup, FormControl, Validators} from '@angular/forms'
+
+import { OffersService } from '../../_services/offers.service';
+import { Offer } from 'src/app/model/offer';
+import { Coordinates } from 'src/app/model/coordinates';
+
+declare const google: any;
 
 @Component({
   selector: 'app-create-offer',
@@ -7,9 +14,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateOfferComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild("search")
+  public searchLocaltion: ElementRef;
+
+  formdata;
+  offer: Offer;
+  coordinates: Coordinates;
+
+  constructor(
+    private offerService: OffersService
+  ) { }
 
   ngOnInit() {
+    this.formdata = new FormGroup({
+      offerTitle: new FormControl('', Validators.compose([
+         Validators.required,
+         Validators.minLength(6),
+         Validators.pattern('^[a-zA-Z]+$')
+      ])),
+      offerLocation: new FormControl('')
+   });
+   this.getPlaceAutocomplete();
+  }
+
+  private getPlaceAutocomplete() {
+    let autocomplete = new google.maps.places.Autocomplete(this.searchLocaltion.nativeElement,
+      {
+        componentRestrictions: { country: 'CL' },
+        types: ['establishment','address', 'geocode']  // 'establishment' / 'address' / 'geocode'
+    });
+    google.maps.event.addListener(autocomplete, 'place_changed', () => {
+        const place = autocomplete.getPlace();
+        console.log(place);
+    });
+  }
+
+  createOffer(data){
+    console.log(data);
+    this.offer.title = data.offerTitle;
+    this.coordinates.latitude = 0;
+    this.coordinates.longitude = 0;
+    this.offer.coordinates = this.coordinates
+    this.offerService.createOffer(this.offer).subscribe((data) => {
+      alert("Dirección agregada");
+    },error=>{
+      console.log(error);
+    })
   }
 
 }
--- src/app/_services/offers.service.ts
+++ src/app/_services/offers.service.ts
@@ -3,6 +3,14 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, of, Subject } from 'rxjs';
 import { Offer } from '../model/offer';
 import { environment } from 'src/environments/environment';
+import { HttpHeaders } from '@angular/common/http';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': "application/json",
+    'Authorization': ""
+  })
+};
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +22,10 @@ export class OffersService {
 
   getOffers(): Observable<Array<Offer>> {
     return this._http.get<Array<Offer>>(`${environment.apiUrl}/offer`);
+  }
+
+  createOffer(offer: Offer): Observable<Offer>{
+    httpOptions.headers = httpOptions.headers.set('Authorization', 'bearer ' + JSON.parse(localStorage.getItem('currentUser')).access_token);
+    return this._http.post<Offer>(`${environment.apiUrl}/offer`, offer, httpOptions)
   }
 }

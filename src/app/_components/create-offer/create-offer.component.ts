ba1f56658c19a979f0fa963f67e348e680d31242
Josse Niño <jossenino@gmail.com>
2020-05-01 23:49:49 -0400

    JN - Modifying offer page to get latitud and longitude from a place

--- src/app/_components/create-offer/create-offer.component.ts
+++ src/app/_components/create-offer/create-offer.component.ts
@@ -4,6 +4,7 @@ import { FormGroup, FormControl, Validators} from '@angular/forms'
 import { OffersService } from '../../_services/offers.service';
 import { Offer } from 'src/app/model/offer';
 import { Coordinates } from 'src/app/model/coordinates';
+import { Address } from 'src/app/model/address';
 
 declare const google: any;
 
@@ -18,8 +19,9 @@ export class CreateOfferComponent implements OnInit {
   public searchLocaltion: ElementRef;
 
   formdata;
-  offer: Offer;
-  coordinates: Coordinates;
+  coordinates: Coordinates = {latitude: 0, longitude: 0};
+  offer: Offer = {title: '', coordinates: this.coordinates};
+  address: Address = {countryName: '', regionName: '', cityName: '', streetName: '', streetNumber: 0}
 
   constructor(
     private offerService: OffersService
@@ -30,9 +32,12 @@ export class CreateOfferComponent implements OnInit {
       offerTitle: new FormControl('', Validators.compose([
          Validators.required,
          Validators.minLength(6),
-         Validators.pattern('^[a-zA-Z]+$')
+         Validators.pattern('^[a-zA-Z0-9 ]*$')
       ])),
-      offerLocation: new FormControl('')
+      offerLocation: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(6)
+     ]))
    });
    this.getPlaceAutocomplete();
   }
@@ -41,25 +46,58 @@ export class CreateOfferComponent implements OnInit {
     let autocomplete = new google.maps.places.Autocomplete(this.searchLocaltion.nativeElement,
       {
         componentRestrictions: { country: 'CL' },
-        types: ['establishment','address', 'geocode']  // 'establishment' / 'address' / 'geocode'
+        types: ['establishment', 'geocode']  // 'establishment' / 'address' / 'geocode'
     });
     google.maps.event.addListener(autocomplete, 'place_changed', () => {
         const place = autocomplete.getPlace();
-        console.log(place);
+        this.getLatitudeLongitude(place);
+        this.getAddressInformation(place);
+        this.getAdditionalInformation(place);
     });
   }
 
   createOffer(data){
-    console.log(data);
     this.offer.title = data.offerTitle;
-    this.coordinates.latitude = 0;
-    this.coordinates.longitude = 0;
     this.offer.coordinates = this.coordinates
+    console.log(this.offer);
     this.offerService.createOffer(this.offer).subscribe((data) => {
       alert("Dirección agregada");
     },error=>{
       console.log(error);
     })
+  }
+
+  getLatitudeLongitude(place:any){
+    this.coordinates.latitude = place.geometry.location.lat();
+    this.coordinates.longitude = place.geometry.location.lng();
+  }
+
+  getAddressInformation(place: any){
+    for (let i = 0; i < place.address_components.length; i++) {
+      const addressType = place.address_components[i].types[0];
+      if (addressType == 'street_number') {
+        console.log('street numer -> ' + place.address_components[i].long_name)
+        this.address.streetNumber = place.address_components[i].long_name
+      }else if (addressType == 'route') {
+        console.log('street name -> ' + place.address_components[i].long_name)
+        this.address.streetName = place.address_components[i].long_name
+      }else if (addressType == 'locality') {
+        console.log('locality -> ' + place.address_components[i].long_name)
+        this.address.cityName = place.address_components[i].long_name
+      }else if (addressType == 'administrative_area_level_1') {
+        console.log('administrative_area_level_1 -> ' + place.address_components[i].long_name)
+        this.address.regionName = place.address_components[i].long_name
+      }else if (addressType == 'country') {
+        console.log('country name -> ' + place.address_components[i].long_name)
+        this.address.cityName = place.address_components[i].long_name
+      }
+    }
+  }
+
+  getAdditionalInformation(place: any){
+    console.log('place id -> ', place.place_id);
+    console.log('reference -> ', place.reference);
+    console.log('url -> ', place.url);
   }
 
 }
---
+++ src/app/model/address.ts
@@ -0,0 +1,7 @@
+export class Address {
+    public countryName: string;
+    public regionName: string;
+    public cityName: string;
+    public streetName: string;
+    public streetNumber: number;
+}
